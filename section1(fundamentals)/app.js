f955c94434037f02dc60e2743f327585d3fbe6ac
muhwezideo <aggrey256@gmail.com>
2018-10-07 22:37:31 +0200

    added console log methods

--- section1(fundamentals)/app.js
+++ section1(fundamentals)/app.js
@@ -1,1 +1,10 @@
-alert('helloworld');
+// alert('helloworld');
+console.log(123)
+console.log("hey dee")
+console.log([2,3,7,0])
+console.table({a:1,b:2})
+console.error()
+console.clear()
+console.warn()
+console.time()
+console.timeEnd()
