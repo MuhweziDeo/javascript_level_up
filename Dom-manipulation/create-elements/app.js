8b45dc610ff4f55826cdd08d6b887aab255da8a2
muhwezideo <aggrey256@gmail.com>
2018-12-08 16:13:35 +0300

    create elements

---
+++ Dom-manipulation/create-elements/app.js
@@ -0,0 +1,7 @@
+const li=document.createElement('li');
+console.log(li);
+li.className="itens";
+li.innerText="hey";
+li.id="unique";
+li.setAttribute('class-2','try');
+li.appendChild(document.createTextNode('Hello world'));
