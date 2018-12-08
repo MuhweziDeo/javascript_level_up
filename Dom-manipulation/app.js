4dad381a4c2923bb5a240548d4d462948a96b8a1
muhwezideo <aggrey256@gmail.com>
2018-12-08 15:29:38 +0300

    query selectors and style manipulation

---
+++ Dom-manipulation/app.js
@@ -0,0 +1,29 @@
+// console.log(document.getElementById("text"));
+// // get things from elements
+// console.log(document.getElementById("text").id);
+// console.log(document.getElementById("text").className);
+// const text=document.getElementById('text')
+// // changing styles
+// text.style.background="red";
+// text.style.color="white";
+// text.style.padding="30px";
+
+
+// // change content
+// document.getElementById('heading').textContent="Heading2";
+// document.getElementById('heading').innerText="heading3"; 
+
+// insert html
+// text.innerHTML=
+// "<span style='background:black;'>hey am new</span>";
+
+
+// document.queryselector()
+console.log(document.querySelector('#text'));
+
+console.log(document.querySelector('h1'));
+
+document.querySelector('h1').style.color='red';
+
+
+document.querySelector('div h1').style.color='green';
