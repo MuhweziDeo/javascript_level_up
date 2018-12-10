402a282779fe1258a8a6461538d21ef633d4b3d1
muhwezi deo <aggrey256@gmail.com>
2018-12-10 18:17:12 +0300

    more oop staff

--- oop/app.js
+++ oop/app.js
@@ -1,21 +1,43 @@
-function Person(name,age,sex,){
-    this.name=name;
-    this.sex=sex;
-    this.age=age
-};
+// function Person(name,age,sex,){
+//     this.name=name;
+//     this.sex=sex;
+//     this.age=age
+// };
 
-function Job(title,description,salary){
-    this.title=title;
-    this.description=description;
+// function Job(title,description,salary){
+//     this.title=title;
+//     this.description=description;
 
-    this.salary=function (){
-        console.log(`${title} and ${salary}`)
-    };
+//     this.salary=function (){
+//         console.log(`${title} and ${salary}`)
+//     };
 
-};
-var deo=new Job('manager',"intelligent",5000)
-console.log(deo.salary())
+// };
+// var deo=new Job('manager',"intelligent",5000)
+// console.log(deo.salary())
 
-var dee=new Person('dee',14,'male');
-console.log(dee.name,dee.age)
+// var dee=new Person('dee',14,'male');
+// console.log(dee.name,dee.age)
 
+
+// person constructor
+function Person(firstname,lname){
+    this.firstname=firstname;
+    this.lname=lname;
+
+}
+
+// greeting
+Person.prototype.greeting=function(){
+    return `Heloo ${this.firstname} ${this.lname}`
+}
+
+const person1=new Person('dee','aggrey');
+console.log(person1)
+
+function Customer(firstname,lname,phone,memebership){
+    Person.call(this,firstname,lname);
+    this.phone=phone;
+    this.memebership=memebership
+
+} 
