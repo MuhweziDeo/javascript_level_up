90e2d9a6926e8837e9637b1fc624567a6893ff8c
muhwezi deo <aggrey256@gmail.com>
2018-12-10 19:58:25 +0300

    add classes

--- oop/app.js
+++ oop/app.js
@@ -21,32 +21,58 @@
 
 
 // person constructor
-function Person(firstname,lname){
-    this.firstname=firstname;
+// function Person(firstname,lname){
+//     this.firstname=firstname;
+//     this.lname=lname;
+
+// }
+
+// // greeting
+// Person.prototype.greeting=function(){
+//     return `Heloo ${this.firstname} ${this.lname}`
+// }
+
+// const person1=new Person('dee','aggrey');
+// console.log(person1)
+// // inherit person prototype methods
+// Customer.prototype=Object.create(Person.prototype);
+// // make customer prototype return customer
+// Customer.prototype.constructor=Customer;
+// function Customer(firstname,lname,phone,memebership){
+//     Person.call(this,firstname,lname);
+//     this.phone=phone;
+//     this.memebership=memebership
+
+// } 
+// const customer1=new Customer('tom','smith','555','standard');
+// // prototype is used to add new properties
+// Customer.prototype.greeting=function(){
+//     return `Hey new ${firstname}`
+// }
+// console.log(customer1.greeting);  
+
+// classes
+class Person{
+constructor(fname,lname,dob){
+    this.fname=fname;
     this.lname=lname;
-
+    this.birthday=new Date(dob);
+}
+greeting(){
+    return `Heloo there ${this.fname}`
+};
+calculateAge(){
+    const diff=Date.now() - this.birthday.getTime();
+    const ageDate=new Date(diff);
+    return Math.abs(ageDate.getFullYear()-1970);
+};
+getsMarried(newName){
+    this.lname=newName;
 }
 
-// greeting
-Person.prototype.greeting=function(){
-    return `Heloo ${this.firstname} ${this.lname}`
-}
+};
 
-const person1=new Person('dee','aggrey');
-console.log(person1)
-// inherit person prototype methods
-Customer.prototype=Object.create(Person.prototype);
-// make customer prototype return customer
-Customer.prototype.constructor=Customer;
-function Customer(firstname,lname,phone,memebership){
-    Person.call(this,firstname,lname);
-    this.phone=phone;
-    this.memebership=memebership
+const mary=new Person('Mary','wiilla',11-13-1980);
 
-} 
-const customer1=new Customer('tom','smith','555','standard');
-// prototype is used to add new properties
-Customer.prototype.greeting=function(){
-    return `Hey new ${firstname}`
-}
-console.log(customer1.greeting);  
+mary.getsMarried('dee');
+console.log(mary.lname); 
