17ecf3b31f2793a94e32549d6fa988fd5618fe10
muhwezi deo <aggrey256@gmail.com>
2018-12-10 08:38:00 +0300

    basic opp

---
+++ oop/app.js
@@ -0,0 +1,13 @@
+function Person(name,age,sex,){
+    this.name=name;
+    this.sex=sex;
+    this.age=age
+};
+
+function Job(title,description,salary){
+
+}
+
+var dee=new Person('dee',14,'male');
+console.log(dee.name,dee.age)
+
